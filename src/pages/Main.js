d7bd37f6670e133d59869783c4deb2d02347cca1
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-02-18 02:36:53 +0900

    update Nexus

---
+++ src/pages/Main.js
@@ -0,0 +1,36 @@
+//import Library
+import React from "react"
+import styled from "styled-components";
+import {useDispatch, useSelector} from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+//import Actions
+
+
+//import elements
+import { Button, Grid, Input, Image, Text } from "../elements" 
+
+//import Icon
+
+
+// impot Component
+
+//import Actions
+
+
+//import axios
+import instance from "../shared/Request";
+
+
+function Main() {
+    
+    return (
+        <Grid>
+            <h1>하이?</h1>
+        </Grid>
+
+    );
+}
+
+
+export default Main;
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,9 +1,21 @@
+//import Library
+import React from "react"
+import { Route, Routes } from "react-router-dom";
+
+
+//import CSS
 import './App.css';
+
+//import Pages
+import Main from "../pages/Main";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <React.Fragment>
+      <Routes>
+        <Route path="/" element={<Main />}/>
+      </Routes>
+    </React.Fragment>
   );
 }
 
