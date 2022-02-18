942e4cfd5d6fb4546a1ed7674607157a8bce681d
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-02-18 14:44:53 +0900

    update Logo& Title and create Login page

--- src/shared/App.js
+++ src/shared/App.js
@@ -8,12 +8,14 @@ import './App.css';
 
 //import Pages
 import Main from "../pages/Main";
+import Login from "../pages/Login";
 
 function App() {
   return (
     <React.Fragment>
       <Routes>
         <Route path="/" element={<Main />}/>
+        <Route path="/login" element={<Login />}/>
       </Routes>
     </React.Fragment>
   );
